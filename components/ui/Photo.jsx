6d2ec859cb03149b6e03e1f7bc8b570a9898053f
Mohammed Reda Belfaida <Mohammed-reda.belfaida@grenoble-inp.org>
2024-10-12 15:35:34 +0200

    new commit

--- components/ui/Photo.jsx
+++ components/ui/Photo.jsx
@@ -15,6 +15,7 @@ const Photo = () => {
         }}
         >
             {/* Image here */}
+
             <motion.div 
             initial={{opacity:0}}
             animate={{
@@ -22,7 +23,7 @@ const Photo = () => {
                 transition:{delay:2.4, duration:0.4,ease:"easeInOut"},
             }}
             className="w-[298px] h-[298px] xl:w-[498px]
-            xl:h-[498px] mix-blend-lighten"
+            xl:h-[498px] mix-blend-lighten absolute"
             >
                 <Image
                     src="/assets/photo2.png"
@@ -33,10 +34,36 @@ const Photo = () => {
                     className="object-contain"
                 />
             </motion.div>
+
+
             {/* Animation circle */}
-            
+            <motion.svg
+                className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
+                fill="transparent"
+                viewBox="0 0 506 506"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+            <motion.circle
+             cx="253"
+             cy="253"
+             r="250"
+             stroke="#00ff99"
+             strokeWidth="4"
+             strokeLinecap="round"
+             strokeLinejoin="round"
+             initial={{strokeDasharray: "24 10 0 0"}}
+             animate={{
+                strokeDasharray:["15 120 25 25","16 25 92 72","4 250 22 22"],
+                rotate:[120,360]
+             }}
+             transition={{
+                duration: 20,
+                repeat: Infinity,
+                repeatType: "reverse",
+             }}
+            />
 
-
+            </motion.svg>
         </motion.div>
     </div>
   )
